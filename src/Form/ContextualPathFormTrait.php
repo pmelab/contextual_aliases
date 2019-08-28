96c8ec1b7e892d8cea1e4e9dfeccdef9edfd2164
Aliaksandr Monich <monya169@gmail.com>
2019-08-28 09:29:35 +0300

    Change validation

--- src/Form/ContextualPathFormTrait.php
+++ src/Form/ContextualPathFormTrait.php
@@ -23,6 +23,50 @@ trait ContextualPathFormTrait {
   /**
    * {@inheritdoc}
    */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $source = &$form_state->getValue('source');
+    $source = $this->aliasManager->getPathByAlias($source);
+    $alias = &$form_state->getValue('alias');
+
+    // Trim the submitted value of whitespace and slashes. Ensure to not trim
+    // the slash on the left side.
+    $alias = rtrim(trim(trim($alias), ''), "\\/");
+
+    if ($source[0] !== '/') {
+      $form_state->setErrorByName('source', 'The source path has to start with a slash.');
+    }
+    if ($alias[0] !== '/') {
+      $form_state->setErrorByName('alias', 'The alias path has to start with a slash.');
+    }
+
+    // Language is only set if language.module is enabled, otherwise save for all
+    // languages.
+    $langcode = $form_state->getValue('langcode', LanguageInterface::LANGCODE_NOT_SPECIFIED);
+
+    if ($this->aliasStorage->aliasExists($alias, $langcode, $this->path['source'], $form_state->getValue('context'))) {
+      $stored_alias = $this->aliasStorage->load(['alias' => $alias, 'langcode' => $langcode, 'context' => $form_state->getValue('context')]);
+      if ($stored_alias['alias'] !== $alias) {
+        // The alias already exists with different capitalization as the default
+        // implementation of AliasStorageInterface::aliasExists is
+        // case-insensitive.
+        $form_state->setErrorByName('alias', t('The alias %alias could not be added because it is already in use in this language with different capitalization: %stored_alias.', [
+          '%alias' => $alias,
+          '%stored_alias' => $stored_alias['alias'],
+        ]));
+      }
+      else {
+        $form_state->setErrorByName('alias', t('The alias %alias is already in use in this language.', ['%alias' => $alias]));
+      }
+    }
+
+    if (!$this->pathValidator->isValid(trim($source, '/'))) {
+      $form_state->setErrorByName('source', t("Either the path '@link_path' is invalid or you do not have access to it.", ['@link_path' => $source]));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Remove unnecessary values.
     $form_state->cleanValues();
